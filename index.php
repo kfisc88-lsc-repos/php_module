59eca8b809cf67f1200ae5feb5d4da3143808142
Krfisc88 <krfisc@outlook.com>
2019-11-27 01:54:49 -0600

    Changed html files to PHP files

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include 'templates/header.html'; ?>
+<?php include 'templates/navbar.html'; ?>
+<main>
+    <h2 class="heading" id="home_heading">Visit Littlefork</h2>
+    <img id="home_image" src="assets/littlefork1.jpg" alt="whiteout at lake Kabetogama"
+    onmouseover="changeImage();" onmouseout="changeImageBack();">
+</main>
+
+<aside>
+</aside>
+<?php include 'templates/footer.html'; ?>
---
+++ javascript.php
@@ -0,0 +1,8 @@
+<?php include 'templates/header.html'; ?>
+<?php include 'templates/navbar.html'; ?>
+  <main>
+  </main>
+
+  <aside>
+  </aside>
+<?php include 'templates/footer.html'; ?>
